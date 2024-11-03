d61fcf9f5613c6a70aac8dd78284f4ef0ad60f8a
Saliazar17 <jsiliezarmoreno123@gmail.com>
2024-11-02 19:32:36 -0600

    second commit of Galery

--- data/hero/hero.js
+++ data/hero/hero.js
@@ -1,64 +1,64 @@
 export const getHeroData = ()=>({
     productos: [
         {
-            imgAlt: 'Producto 1',
+            imgAlt: 'Imagen 1',
             imgUrl: 'https://picsum.photos/id/120/300/200',
         },
         {
-            imgAlt: 'Producto 2',
+            imgAlt: 'Imagen 2',
             imgUrl: 'https://picsum.photos/id/130/300/200',
         },
         {
-            imgAlt: 'Producto 3',
+            imgAlt: 'Imagen 3',
             imgUrl: 'https://picsum.photos/id/140/300/200',
         },
         {
-            imgAlt: 'Producto 4',
+            imgAlt: 'Imagen 4',
             imgUrl: 'https://picsum.photos/id/151/300/200',
         },
         {
-            imgAlt: 'Producto 5',
-            imgUrl: 'https://picsum.photos/id/159/300/200',
+            imgAlt: 'Imagen 5',
+            imgUrl: 'https://picsum.photos/id/169/300/200',
         },
         {
-            imgAlt: 'Producto 6',
-            imgUrl: 'https://picsum.photos/id/120/300/200',
+            imgAlt: 'Imagen 6',
+            imgUrl: 'https://picsum.photos/id/16/300/200',
         },
         {
-            imgAlt: 'Producto 7',
-            imgUrl: 'https://picsum.photos/id/130/300/200',
+            imgAlt: 'Imagen 7',
+            imgUrl: 'https://picsum.photos/id/60/300/200',
         },
         {
-            imgAlt: 'Producto 8',
-            imgUrl: 'https://picsum.photos/id/140/300/200',
+            imgAlt: 'Imagen 8',
+            imgUrl: 'https://picsum.photos/id/90/300/200',
         },
         {
-            imgAlt: 'Producto 9',
-            imgUrl: 'https://picsum.photos/id/151/300/200',
+            imgAlt: 'Imagen 9',
+            imgUrl: 'https://picsum.photos/id/81/300/200',
         },
         {
-            imgAlt: 'Producto 10',
-            imgUrl: 'https://picsum.photos/id/159/300/200',
+            imgAlt: 'Imagen 10',
+            imgUrl: 'https://picsum.photos/id/135/300/200',
         },
         {
-            imgAlt: 'Producto 11',
-            imgUrl: 'https://picsum.photos/id/120/300/200',
+            imgAlt: 'Imagen 11',
+            imgUrl: 'https://picsum.photos/id/192/300/200',
         },
         {
-            imgAlt: 'Producto 12',
-            imgUrl: 'https://picsum.photos/id/120/300/200',
+            imgAlt: 'Imagen 12',
+            imgUrl: 'https://picsum.photos/id/72/300/200',
         },
         {
-            imgAlt: 'Producto 13',
-            imgUrl: 'https://picsum.photos/id/120/300/200',
+            imgAlt: 'Imagen 13',
+            imgUrl: 'https://picsum.photos/id/183/300/200',
         },
         {
-            imgAlt: 'Producto 14',
-            imgUrl: 'https://picsum.photos/id/120/300/200',
+            imgAlt: 'Imagen 14',
+            imgUrl: 'https://picsum.photos/id/31/300/200',
         },
         {
-            imgAlt: 'Producto 15',
-            imgUrl: 'https://picsum.photos/id/120/300/200',
+            imgAlt: 'Imagen 15',
+            imgUrl: 'https://picsum.photos/id/127/300/200',
         },
     ]
 })
